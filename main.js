c47fb43f0af54e6b715f36e35a669dae9f2e5216
Rhemie Kekana@ <rhemiemkekana@gmail.com>
2024-08-12 18:31:26 +0200

    Added Night shift feature

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const daysArray = [];
 for (let i = 1; i <= 365; i += 4) {
   daysArray.push([i, i + 1, i + 2, i + 3]);
 }
+console.log(`Days array: ${daysArray}`);
 
 // setting search date
 let dateSearch = document.querySelector('input[type="date"]');
@@ -19,15 +20,17 @@ let today = new Date();
 let Difference_In_Time;
 let Difference_In_Days;
 let remainingDays;
+let daysIndex;
 let quotes = [];
 
 calculateDays(startDay, today);
 
 function getDate(){
   let searchDate = dateSearch.value;
-  console.log(searchDate);
+  console.log(`Search date: ${searchDate}`);
   today =  new Date(searchDate);
   calculateDays(startDay, today);
+  console.log(`Today: ${today}`)
   return today;
 }
 
@@ -36,10 +39,16 @@ function calculateDays(){
   Difference_In_Days = Math.round(Difference_In_Time / (1000 * 3600 * 24));
   //find index of number of days from daysArray
   let searchNumber = Difference_In_Days;
+  console.log(`SearchNumber: ${searchNumber}`);
   let index = daysArray.findIndex((subArray) => subArray.includes(searchNumber));
-  let daysIndex = daysArray[index].indexOf(searchNumber);
+  console.log(`Index: ${index}`);
+  daysIndex = daysArray[index].indexOf(searchNumber);
+  console.log(`DaysIndex: ${daysIndex}`);
   remainingDays = daysArray[index].length - (daysIndex);
+  console.log(`RemainingDays: ${remainingDays}`);
   printResults(index);
+  console.log(daysArray);
+  console.log(index);
   return index;
 }
 
@@ -51,11 +60,20 @@ function printResults(index){
     message.innerHTML = "Breathe. Relax. Pull yourself towards yourself.";
     image.src="./1694_U1RVRElPIEtBVCAzOTItMTg.jpg";
 } else {
+   //check if it's the third or fourth day first
+   if (daysIndex > 1){
+    stat.innerHTML = "We are working night shift!";
+    date.innerHTML = `${today.toLocaleDateString('en-ZA', { weekday: 'short' })} ${today.getDate()}/${(today.getMonth() + 1).toString().padStart(2, '0')}/${today.getFullYear()}`;
+    remaining.innerHTML = `${remainingDays} days left`;
+    message.innerHTML = "Time to tackle the daily grind."
+    image.src = "./night-shift.png";
+   } else {
     stat.innerHTML = "We are working!";
     date.innerHTML = `${today.toLocaleDateString('en-ZA', { weekday: 'short' })} ${today.getDate()}/${(today.getMonth() + 1).toString().padStart(2, '0')}/${today.getFullYear()}`;
     remaining.innerHTML = `${remainingDays} days left`;
     message.innerHTML = "Time to tackle the daily grind."
     image.src = "./Businessman run for going back work.jpg";
+   }
 }
 }
 
